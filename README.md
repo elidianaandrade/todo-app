2b530f51659c38b361ab0995b8c3ef7660b46a97
elidianaandrade <lide_andrade_redes@hotmail.com>
2023-01-14 10:38:36 -0300

    add class and connection

---
+++ README.md
@@ -0,0 +1,18 @@
+# TODO APP
+
+## 🎬 Sobre o projeto
+ Todo App desenvolvido em Java e Gradle para o programa Start da Capgemini.
+
+## 💻 Tecnologias
+![Java](https://img.shields.io/badge/Java-000?style=for-the-badge&logo=java&logoColor=7520FF)
+![Gradle](https://img.shields.io/badge/Gradle-000?style=for-the-badge&logo=grade&logoColor=7520FF)
+
+## ⚙ Funcionalidades
+
+- [x] Criar um novo projeto;
+- [x] Criar uma nova tarefa (vinculada a um projeto).
+
+## 🔍 Referências
+
+- [Repositório do Instrutor Marcio Michelluzzi](https://startcapgemini.com.br/)
+- [Start By Capgemini](https://startcapgemini.com.br/).
---
+++ src/main/java/com/ea/todoApp/util/ConnectionFactory.java
@@ -0,0 +1,46 @@
+package com.ea.todoApp.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/todoapp?characterEncoding=utf8";
+    public static final String USER = "root";
+    public static final String PASS = "admin";
+
+    public static java.sql.Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw new RuntimeException("Erro na conexão com o Banco de Dados", ex);
+        }
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao fechar a conexão com o Banco de Dados", ex);
+        }
+    }
+
+    public static void closeConnection(Connection connection, PreparedStatement statement) {
+        closeConnection(connection);
+        try {
+            if (statement != null) {
+                statement.close();
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao fechar a conexão com o Banco de Dados", ex);
+        }
+
+    }
+
+}
